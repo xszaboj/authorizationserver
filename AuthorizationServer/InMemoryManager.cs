c0db7f5acc9f63a2d81e50bec35d2e51692ff793
Jack Szabo <Jack Szabo>
2016-05-31 22:03:32 +0100

    initial commit

---
+++ AuthorizationServer/InMemoryManager.cs
@@ -0,0 +1,68 @@
+﻿using IdentityServer3.Core;
+using IdentityServer3.Core.Models;
+using IdentityServer3.Core.Services.InMemory;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Web;
+
+namespace AuthorizationServer
+{
+    public class InMemoryManager
+    {
+        public List<InMemoryUser> Users()
+        {
+            return new List<InMemoryUser>()
+            {
+                new InMemoryUser
+                {
+                    Subject = "test@mail.com",
+                    Username = "test@mail.com",
+                    Password = "test",
+                    Claims = new [] {
+                        new Claim(Constants.ClaimTypes.Name, "test name")
+                    }
+                }
+            };
+        }
+
+        public IEnumerable<Client> Clients()
+        {
+            return new[]
+            {
+                new Client
+                {
+                    ClientId = "social network",
+                    ClientSecrets = new List<Secret>
+                    {
+                        new Secret("secret".Sha256())
+                    },
+                    ClientName = "SocialNetwork",
+                    Flow = Flows.ResourceOwner,
+                    AllowedScopes = new List<string>
+                    {
+                        Constants.StandardScopes.OpenId,
+                        "read"
+                    },
+                    Enabled = true
+                }
+            };
+        }
+
+        public IEnumerable<Scope> Scopes()
+        {
+            return new[]
+            {
+                StandardScopes.OpenId,
+                StandardScopes.Profile,
+                StandardScopes.OfflineAccess,
+                new Scope {
+                    Name = "read",
+                    DisplayName = "read user name"
+                }
+            };
+        }
+
+    }
+}
---
+++ AuthorizationServer/Startup.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Owin;
+using IdentityServer3.Core.Configuration;
+
+[assembly: OwinStartup(typeof(AuthorizationServer.Startup))]
+
+namespace AuthorizationServer
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=316888
+            var options = new IdentityServerOptions();
+            SetOptions(options);
+            app.UseIdentityServer(options);
+        }
+
+        private void SetOptions(IdentityServerOptions options)
+        {
+            //var certificate = Convert.FromBase64String(System.Configuration.ConfigurationManager.AppSettings["SigningCertificate"]);
+            //string password = System.Configuration.ConfigurationManager.AppSettings["SigningCertificatePwd"];
+            options.SigningCertificate = new System.Security.Cryptography.X509Certificates.X509Certificate2(@"C:\temp\combined.pfx", "password");
+            options.RequireSsl = false; //do not use https
+
+            var manager = new InMemoryManager();
+            options.Factory = new IdentityServerServiceFactory()
+                .UseInMemoryUsers(manager.Users())
+                .UseInMemoryClients(manager.Clients())
+                .UseInMemoryScopes(manager.Scopes());
+        }
+    }
+}
